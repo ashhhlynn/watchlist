aed0fd6bce80b501217f857f103660df8ab1617b
ashhhlynn <ashleylynnwelch@gmail.com>
2022-09-22 02:38:34 -0500

    made user views, put in signup form for user signup view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+    
+    get '/users' do 
+    @users = User.all 
+    erb :'users/index'
+    end
+
+    get '/users/:id' do 
+    @user = User.find_by(id: params[:id])
+    erb :'users/show'
+    end    
+    
+    get '/signup' do
+    erb :'users/signup'
+    end
+
