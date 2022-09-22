d39904a996e23330dd44c3503936a4baa2827a53
ashhhlynn <ashleylynnwelch@gmail.com>
2022-09-22 03:19:59 -0500

    added get watchings new method and post watchings method, get edit watchings method, patch watchings method, and delete watchings method

--- app/controllers/watchings_controller.rb
+++ app/controllers/watchings_controller.rb
@@ -10,7 +10,40 @@ get '/watchings/:id' do
     erb :'watchings/show'
 end
 
+get '/watchings/new' do 
+    erb :'watchings/new'
+    end
 
+post '/watchings' do
+    if params[:watching][:title] == "" || params[:watching][:image_url] == "" || params[:watching][:movie_or_show] == "" || params[:watching][:streaming_location] == ""
+        erb :'watchings/new'
+    else     
+        @watching = Watching.create(params[:watching])
+        @watching.creator_id = current_user.id
+        @watching.save #'do I need that?'
+        redirect '/watchings'
+    end 
+    end 
 
+    get '/watchings/:id/edit' do
+    @watching = Watching.find_by(id: params[:id])
+    erb :'watchings/edit'
+    end   
+        
+    patch '/watchings/:id' do
+    @watching = Watching.find_by(id: params[:id])
+    if params[:watching][:title] != "" && params[:watching][:image_url] != "" && params[:watching][:movie_or_show] != "" && params[:watching][:streaming_location] != ""
+    @watching.update(params[:watching])
+    redirect "/watchings/#{@watching.id}"
+    else 
+    erb :'watchings/edit'
+    end 
+    end 
+        
+    delete '/watchings/:id/delete' do
+    @watching = Watching.find_by(id: params[:id])
+    @watching.destroy
+    redirect to '/watchings'
+    end    
 
 end 
